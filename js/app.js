b3232ab44d7d79d8ad2cc8f0cd36fe4561253cee
Jonathan E <jonathan_east@ymail.com>
2018-07-21 13:50:31 -0400

    Added code to appendPageLinks function

--- js/app.js
+++ js/app.js
@@ -24,13 +24,30 @@ showPage(1); //Currently testing pageNumber to ensure program is working
 
 // Create a function named 'appendPageLinks' that accepts a parameter for the studentList
 function appendPageLinks(studentList) {
-  //Need to determine how many pages needed for the student list.
+  //Need to determine how many pages needed for the student list.  In this case,
+  //there are 54 students on the list and there will need to be a total of 6
+  //pages needed.
+  let $pages = $('.student-list').size / 10;
   //Create a page link section
+  let $newDiv = document.createElement('div');
+  $($newDiv).addClass('pagination');
+  $('div').append($newDiv);
+
+  let $newUL = document.createElement('ul');
+  $('.pagination').append($newUL);
+
+  let $newLI = document.createElement('li');
+  $('ul').append($newLI);
   //Create a 'for' loop for every page
+    for(let i = 0; i < $pages.length; i++) {
     //add a page link to the page link section
+    $('.pagination li').append('<a href="#">+(i+1)+</a>');
+    }
   //remove the old page link section from the site
   //append the new page link section to the site
   //define what happens when you click a link (action listener)
     //use the 'showPage' function to display the page for the link clicked
     //mark that link as "active"
 }
+
+appendPageLinks();
